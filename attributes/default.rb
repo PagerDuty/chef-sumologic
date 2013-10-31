960161f169e1f6d0c54928d57e881794503ff19f
Ranjib Dey <ranjib@pagerduty.com>
2013-10-31 14:50:31 -0700

    use sumologic-collectoir recipe for installation, use our own lwrps, matcher

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,10 @@
 #
 # Author:: Luke Kosewski (<luke@pagerduty.com>)
+# Author:: Ranjib Dey (ranjib@pagerduty.com)
 # Cookbook Name:: sumologic
 # Attributes:: default
 #
-# Copyright 2012, PagerDuty, Inc.
+# Copyright 2013, PagerDuty, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,41 +20,6 @@
 #
 
 # default sumocollector attributes
-default[:sumologic][:rootdir] = '/opt/sumologic'
-
-# Sumologic control commands
-default[:sumologic][:start_cmd] = '/etc/init.d/collector start'
-default[:sumologic][:stop_cmd] = '/etc/init.d/collector stop'
-default[:sumologic][:restart_cmd] = '/etc/init.d/collector restart'
-
 # Set to true to disable the collector on this node
 default[:sumologic][:disabled] = false
-
-# The sumocollector version you're installing
-sumover = '19.38-9'
-default[:sumologic][:collector][:version] = sumover
-
-# The sumocollector tarball you wish to install.  You probably don't want to
-# change this; just grab and host the appropriate tarball corresponding to
-# its name.  But be careful since Sumo Logic drops ".tar" on occasion for no
-# reason, turning various releases into just ".gz."
-tarball = "SumoCollector_unix_#{sumover.gsub('.', '_')}.gz"
-default[:sumologic][:collector][:tarball] = tarball
-# The checksum for that tarball
-default[:sumologic][:collector][:checksum] = 'a5a7d841265bb2dd7203fa81109b60a5994bdf187c7e6731ec8421d23f599b61'
-
-# The URL where your tarball is stored
-default[:sumologic][:collector][:url] = "http://path/to/#{tarball}"
-
-# Admin credentials for Sumo Logic account
-default[:sumologic][:admin][:email] = 'user@example.com'
-default[:sumologic][:admin][:pass] = 'mysupersecretpass'
-
-# Set to change the default category for log files if not specified.
-default[:sumologic][:sources][:default_category] = 'log'
-# Set to force logs that don't carry timezones in timestamps or where you want
-# to ignore that data to this timezone (defaults to UTC).
-default[:sumologic][:sources][:default_timezone] = 'UTC'
-# Set to true to force timestamps parsed from logs into this timezone,
-# regardless of whether they carry tz information or not.
-default[:sumologic][:sources][:force_timezone] = false
+default['sumologic']['sources']['default_category'] = 'log'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,11 @@
 #
+# Author:: Ranjib Dey (ranjib@pagerduty.com)
 # Author:: Luke Kosewski (<luke@pagerduty.com>)
+# Contact:: ops+opensource@pagerduty.com
 # Cookbook Name:: sumologic
 # Recipe:: default
 #
-# Copyright 2012, PagerDuty, Inc.
+# Copyright 2013, PagerDuty, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,163 +18,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-directory node[:sumologic][:rootdir] do
-  owner 'root'
-  group 'root'
-  mode 0755
-  recursive true
-end
-
-c = "#{node[:sumologic][:rootdir]}/#{node[:sumologic][:collector][:tarball]}"
-remote_file c do
-  backup false
-  owner 'root'
-  group 'root'
-  mode 0644
-  checksum node[:sumologic][:collector][:checksum]
-  source node[:sumologic][:collector][:url]
-  notifies :run, 'bash[extract_sumologic]', :immediately
-end
-
-collectorbin = '/etc/init.d/collector'
-
-bash 'extract_sumologic' do
-  def wrapperdir
-    if node[:os] == 'linux'
-      case node[:kernel][:machine]
-      when 'i386', 'i586', 'i686'
-        'linux32'
-      when 'x86_64'
-        'linux64'
-      else
-        raise Chef::Exceptions::UnsupportedAction,
-          'I don\'t know how to install SumoLogic on your Linux arch yet.'
-      end
-    else
-      raise Chef::Exceptions::UnsupportedAction,
-        'I don\'t know how to install SumoLogic on your OS yet.'
-    end
-  end
-
-  user 'root'
-  cwd node[:sumologic][:rootdir]
-  code <<-EOH
-    [ -x "#{collectorbin}" ] && "#{collectorbin}" stop
-    tar zxf #{node[:sumologic][:collector][:tarball]}
-    chmod 755 sumocollector/collector
-    cp sumocollector/tanuki/#{wrapperdir}/wrapper sumocollector
-  EOH
-  # The last file to be created.  If it's there, then we default to action
-  # :nothing (though the remote_file above can override this).  If it isn't
-  # there, then we need to untar.
-  if !File.exists?("#{node[:sumologic][:rootdir]}/sumocollector/wrapper")
-    action :run
-  else
-    action :nothing
-  end
-end
-
-bash 'install collector into /etc/*.d' do
-  user 'root'
-  code "#{node[:sumologic][:rootdir]}/sumocollector/collector install"
-  not_if { File.exists?(collectorbin) }
-end
-
-# Note:  we could do everything this resource does inside the .erb file, but
-# erb syntax is way gross.  This depends on the fact that all the setters of
-# sumo_source ran at compile time, because you ran the definition, didn't you?
-ruby_block 'compile_sumo_sources' do
-  block do
-    node.run_state[:sumo_output] = []
-    node.run_state[:sumo_output] =
-      # Stupid hacky .to_s because Ruby 1.8 can't <=> symbols.
-      node.run_state[:sumo_source].sort_by{ |k,v| k.to_s }.map do |k, v|
-        # Forgive the weird indentation - it makes for a much more readable
-        # config file, I assure you.
-        x = <<-EOS.rstrip
-    {
-      "type" : "localWildCard",
-      "name" : "#{k}",
-      "timeZone" : "#{v[:default_timezone]}",
-      "forceTimeZone" : #{v[:force_timezone]},
-      "pathExpression" : "#{v[:path]}",
-      "category" : "#{v[:category]}"
-    }
-        EOS
-      end if node.run_state[:sumo_source]
-  end
-end
-
-selectedjson =
-  "#{node[:sumologic][:rootdir]}/sumocollector/installerSources/selected.json"
-p = template selectedjson do
-  source 'selected.json.erb'
-  mode 0644
-  backup false
-  if node[:sumologic][:disabled]
-    action :nothing  # Don't go writing this template if no collector.
-  else
-    notifies :restart, 'service[collector]'
-  end
-end
-
-file "#{node[:sumologic][:rootdir]}/sumocollector/config/creds/main.properties" do
-  backup false
-  action :nothing
-  subscribes :delete, resources(:template => selectedjson), :immediately
-end
-
-# Had to steal this from inside the tar file because it's awful to modify.
-# Converted to template.
-template "#{node[:sumologic][:rootdir]}/sumocollector/config/wrapper.conf" do
-  source 'wrapper.conf.erb'
-  backup false
-  mode 0644
-  variables(
-    :java_location => '/usr/bin/java',
-    :sumover => node[:sumologic][:collector][:version],
-    :sumo_email => node[:sumologic][:admin][:email],
-    :sumo_pass => node[:sumologic][:admin][:pass],
-    :selectedjson => p
-  )
-  notifies :restart, 'service[collector]' if !node[:sumologic][:disabled]
-end
-
-sumobin = "#{node[:sumologic][:rootdir]}/sumocollector/bin"
-directory sumobin do
-  mode 0755
-end
-
-# This is a horrible hack because Sumo Logic can't reconfigure its sources
-# properly without reuploading a bunch of already-uploaded data.  To prevent
-# that, we run logrotate (this hurts me so much I can't even describe it, I
-# hope they get their shit straight, soon).  --lkosewsk, 30.11.2012.
-sumorestart = "#{sumobin}/restart.sh"
-template sumorestart do
-  source 'restart.sh.erb'
-  backup false
-  mode 0755
-  variables(
-    :start_cmd => node[:sumologic][:start_cmd],
-    :stop_cmd => node[:sumologic][:stop_cmd],
-    :restart_cmd => node[:sumologic][:restart_cmd],
-    :selectedjson => p
-  )
-end
-
-file "#{node[:sumologic][:rootdir]}/sumocollector/config/state" do
-  s = node[:sumologic][:disabled] ? :stop : :restart
-  backup false
-  mode 0644
-  content s.to_s
-  notifies s, 'service[collector]'
-end
-
-service 'collector' do
-  action :nothing
-  supports [:start, :stop, :status, :restart, :install, :remove, :dump,
-            :console, :condrestart]
-  restart_command sumorestart
-end
+include_recipe "sumologic-collector" unless node.sumologic.disabled
