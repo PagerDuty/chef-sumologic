9ed4e9ba49e428bd2f234e33d94a894fad67a8f6
Daniel Silverman <me@agperson.com>
2014-08-28 15:28:00 -0400

    rename in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name             'sumologic'
+name             'bc_sumologic'
 maintainer       'PagerDuty, Inc.'
 maintainer_email 'ranjib@pagerduty.com'
 license          'Apache 2.0'
 description      "Installs/configures Sumo Logic's sumocollector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.2.0'
 
 depends 'sumologic-collector'
 
-recipe 'sumologic', 'Installs Sumo Logic collector, and provides resources for forwarding logs to sumologic'
+recipe 'bc_sumologic', 'Installs Sumo Logic collector, and provides resources for forwarding logs to sumologic'
