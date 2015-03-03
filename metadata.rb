8b86f7987024be1bbfaede36d6e88104cb2d62bd
Grant Ridder <grant@pagerduty.com>
2015-03-02 18:46:27 -0800

    Revert version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ranjib@pagerduty.com'
 license          'Apache 2.0'
 description      "Installs/configures Sumo Logic's sumocollector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.0.0'
 
 depends 'sumologic-collector'
 
