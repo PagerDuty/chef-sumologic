02f5c2076d66ee3202c55f256527f8759fb34887
John Schectman <jschectman@brightcove.com>
2015-06-05 14:01:56 -0400

    bump metadata for next brightcove/pagerduty merge

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'ranjib@pagerduty.com'
 license          'Apache 2.0'
 description      "Installs/configures Sumo Logic's sumocollector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '1.3.0'
 
-depends 'sumologic-collector', '= 1.2.1'
+depends 'sumologic-collector', '= 1.3.0'
 
 recipe 'bc_sumologic', 'Installs Sumo Logic collector, and provides resources for forwarding logs to sumologic'
